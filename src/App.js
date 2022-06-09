47c21409fcd9a1c95cdc0462aae947ad75d6288d
Nicholas H <NicholasH7@hotmail.com>
2022-06-08 20:42:29 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+<div class="wrapper">
+  <div class="hair">
+        <div class="fringe2"></div>
+        <div class="fringe3"></div>
+        <div class="accessories"></div>
+        <div class="accessories2"></div>
+        <div class="accessories3"></div>
+  </div>
+    <div class="head">
+        <div class="eyebrowL"></div>
+        <div class="eyebrowR"></div>
+        <div class="eyeR"></div>
+        <div class="eyeL"></div>
+        <div class="eye-color1"></div>
+        <div class="eye-color2"></div>
+        <div class="fringe"></div>
+        <div class="nose"></div>
+        <div class="chick1"></div>
+        <div class="chick2"></div>
+        <div class="lips"></div>
+         
+    </div>
+
+    <div class="neck">
+        <div class="cone"></div>
+    </div>
+
+
+
+    <div class="shoulder">
+        <div class="armR"></div>
+        <div class="armL"></div>
+        <div class="arm1"></div>
+        <div class="arm2"></div>
+    </div>
+
+    <div class="boobs"></div>
+
+    <div class="skirt1"></div>
+
+    <div class="skirt"></div>
+
+    <div class="legL"></div>
+
+    <div class="legR"></div>
+
+    <div class="boot1"></div>
+
+    <div class="boot2"></div>
+
+</div>
     </div>
   );
 }
